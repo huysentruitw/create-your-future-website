84a50ccd214321b90337ae7860a19f4ea8669d03
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-10-21 21:17:47 +0200

    Highlight current navigation item + fix styles

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,13 +1,31 @@
-function createNavbarCollapsePixelObserver() {
-  const navbarCollapsePixel = document.getElementById('navbar-pixel');
-
-  const observer = new IntersectionObserver(entries => {
-    const shouldCollapse = !entries[0].isIntersecting;
+function useNavbarCollapsePixelObserver() {
+  var navbarCollapsePixel = document.getElementById('navbar-pixel');
+  var observer = new IntersectionObserver(function(entries) {
+    var shouldCollapse = !entries[0].isIntersecting;
     document.querySelector('.navbar.fixed-top').classList.toggle('solid', shouldCollapse);
   });
   observer.observe(navbarCollapsePixel);
 }
 
-window.addEventListener('load', (event) => {
-  createNavbarCollapsePixelObserver();
+function useScrollToBehavior() {
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 750);
+    event.preventDefault();
+  });
+}
+
+function useScrollSpy() {
+  $('body').scrollspy({
+    target: '.navbar.fixed-top',
+    offset: 51
+  });
+}
+
+window.addEventListener('load', function() {
+  useNavbarCollapsePixelObserver();
+  useScrollToBehavior();
+  useScrollSpy();
 });
